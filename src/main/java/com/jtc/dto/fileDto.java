fa20abf7f87e5ea05f1bc43e0ead03e807076d2e
dipankar <pdipankar28@gmail.com>
2025-02-23 16:43:42 +0530

    uploaded

---
+++ src/main/java/com/jtc/dto/fileDto.java
@@ -0,0 +1,42 @@
+package com.jtc.dto;
+
+public class fileDto {
+
+	
+	private Integer id;
+	private String originalFileName;
+	private String displayFileName;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getOriginalFileName() {
+		return originalFileName;
+	}
+	public void setOriginalFileName(String originalFileName) {
+		this.originalFileName = originalFileName;
+	}
+	public String getDisplayFileName() {
+		return displayFileName;
+	}
+	public void setDisplayFileName(String displayFileName) {
+		this.displayFileName = displayFileName;
+	}
+	public fileDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public fileDto(Integer id, String originalFileName, String displayFileName) {
+		super();
+		this.id = id;
+		this.originalFileName = originalFileName;
+		this.displayFileName = displayFileName;
+	}
+
+	
+
+}
